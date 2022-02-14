4535e969db4b5755c047d48f095930bdad56b8ce
iljun <sumusb@naver.com>
2022-02-14 21:23:12 +0900

    init project

---
+++ src/test/java/me/iljun/batch/SpringBatchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.iljun.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
